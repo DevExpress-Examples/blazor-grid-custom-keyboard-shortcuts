0d6ee6c877b60192f318a33ab2e6ef151b3c89db
artemkuzmindevexpress <artem.kuzmin@devexpress.com>
2025-04-22 15:35:44 +0400

    Example Implemented

---
+++ CS/GridCustomShortcuts/Components/Pages/Index.razor.js
@@ -0,0 +1,26 @@
+﻿let keydownHandler = null;
+
+export function addCaptureKeyListener(element, dotNetHelper) {
+    if (!keydownHandler) {
+        keydownHandler = (event) => {
+            if (event.key === "Enter" && event.shiftKey === true && element.contains(event.target)) {
+                event.stopPropagation();
+                dotNetHelper.invokeMethodAsync('HandleKeyDown');
+            }
+
+            if (event.code == "KeyA" && event.ctrlKey === true && element.contains(event.target)) {
+                event.stopPropagation();
+                event.preventDefault();
+                dotNetHelper.invokeMethodAsync('SelectAllRows');
+            }
+        };
+        window.addEventListener('keydown', keydownHandler, true);
+    }
+}
+
+export function removeCaptureKeyListener() {
+    if (keydownHandler) {
+        window.removeEventListener('keydown', keydownHandler, true);
+        keydownHandler = null;
+    }
+}
